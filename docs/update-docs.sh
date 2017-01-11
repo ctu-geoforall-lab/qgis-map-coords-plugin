c217bfe35066a41290ba18189a6ec859301e6fc4
Martin Landa <landa.martin@gmail.com>
2017-01-11 20:44:25 +0100

    fix update-docs.sh script

--- docs/update-docs.sh
+++ docs/update-docs.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
-# build the docs
+set -e
+# build docs
+git checkout gh-pages
+rm -rf *
+touch .nojekyll
+git checkout master docs
+cd docs
 make clean
 make html
 cd ..
-# commit and push
-git add -A
-git commit -m "building and pushing docs"
-git push origin master
-# switch branches and pull the data we want
-git checkout gh-pages
-rm -rf .
-touch .nojekyll
-git checkout master docs/build/html
-mv ./docs/build/html/* ./
-rm -rf ./docs
+mv docs/build/html/* ./
+rm -rf docs
 git add -A
 git commit -m "publishing updated docs..."
 git push origin gh-pages
